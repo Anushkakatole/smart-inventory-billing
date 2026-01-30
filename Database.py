103e789109c7e6582fa992fa43eaf63072a1213e
Anushka Katole <katoleanushka@gmail.com>
2026-01-30 12:24:04 +0530

    Enable SSL for Supabase PostgreSQL connection

--- Database.py
+++ Database.py
@@ -2,13 +2,13 @@ import streamlit as st
 import psycopg2
 
 def connection():
-    conn = psycopg2.connect(
+    return psycopg2.connect(
         host=st.secrets["database"]["host"],
         database=st.secrets["database"]["database"],
         user=st.secrets["database"]["user"],
         password=st.secrets["database"]["password"],
-        port=st.secrets["database"]["port"]
+        port=st.secrets["database"]["port"],
+        sslmode="require"
     )
-    return conn
 
 conn = connection()
